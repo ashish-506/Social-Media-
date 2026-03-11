af82e3e99bb5a2557587e96e5254665ec0d326ce
Ashish <ashish02.aks@gmail.com>
2026-03-11 23:49:03 +0530

    added comment

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -15,6 +15,6 @@ router.get('/personalFeed',isLoggedIn,getPersonalFeed);
 router.post('/create',isLoggedIn,createPost);
 router.post('/like/:id',isLoggedIn,likeHandler);
 router.post('/comment/:id',isLoggedIn,commentHandler);
-router.get('/:id',readPost);
+router.get('/:id',readPost); // dynamic routes ko last me add krna hai 
 
 module.exports = router;
